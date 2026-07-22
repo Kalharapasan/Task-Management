de5749dd3ac542faf5aa9d2fce6a319c9d5d933b
Pasan Kalhara <kalharap84@gmail.com>
2026-07-22 12:10:13 +0530

    Add:Database Configer

---
+++ backend/src/config/db.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST ,
+  port: process.env.DB_PORT ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  dateStrings: true,
+});
+
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log('MySQL connected successfully');
+    connection.release();
+  } catch (error) {
+    console.error('Unable to connect to MySQL:', error.message);
+    process.exit(1);
+  }
+}
+
+module.exports = { pool, testConnection };
